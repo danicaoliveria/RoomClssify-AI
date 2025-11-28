4b4306b1f3ecfe8defd791e2804b768ad742913a
Elijajajah <aguilarjiroelijah@gmail.com>
2025-11-28 09:26:01 +0800

    Update CORS settings

--- server.py
+++ server.py
@@ -18,19 +18,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://danicaoliveria.github.io",
-        "https://danicaoliveria.github.io",
-        "http://localhost:5500",
-        "http://127.0.0.1:5500",
-        "http://localhost:8000",
-        "http://127.0.0.1:8000"
-    ],
-    allow_credentials=True,
+    allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 
 # ---------------------------------------------------
 # CLASS LABELS
